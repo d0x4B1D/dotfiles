9150a7bce968550c1523565b2984fef08b036b55
d0x4b1d <phil@d0x4b1d.eu>
2021-06-24 22:46:20 +0200

    Add zsh auto suggestion plugin

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ fi
 
 git clone -c core.eol=lf -c core.autocrlf=false --depth 1 https://github.com/d0x4B1D/dotfiles.git $HOME/.dotfiles
 git clone -c core.eol=lf -c core.autocrlf=false --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/zsh/plugins/zsh-syntax-highlighting
+git clone -c core.eol=lf -c core.autocrlf=false --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.dotfiles/zsh/plugins/zsh-autosuggestions
 cp $HOME/.dotfiles/config/zshrc $HOME/.zshrc
 
 popd > /dev/null
