951ab1cba714f792eb26e145fa513576db72378f
d0x4b1d <phil@d0x4b1d.eu>
2021-06-24 21:28:16 +0200

    Add an easy way to install .dotfiles

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+#
+# This script should be run via curl:
+#   bash -c "$(curl -fsSL https://raw.githubusercontent.com/d0x4B1D/dotfiles/master/install.sh)"
+# or via wget:
+#   bash -c "$(wget -qO- https://raw.githubusercontent.com/d0x4B1D/dotfiles/master/install.sh)"
 
 pushd $(dirname $0) > /dev/null
 
-cp ./config/zshrc ~/.zshrc
+git clone -c core.eol=lf -c core.autocrlf=false --depth 1 https://github.com/d0x4B1D/dotfiles.git ~/.dotfiles
+cp ~/.dotfiles/config/zshrc ~/.zshrc
 
 popd > /dev/null
-
